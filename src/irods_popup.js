ac94a6a9186b5457482062deb2253ec7fa69b1e1
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2020-01-13 11:40:44 +0100

    removed slash from path

--- src/irods_popup.js
+++ src/irods_popup.js
@@ -112,9 +112,9 @@ import App from './App';
          const TEMPLATE = <div className="detailFileInfoContainer">
                           <div className="mainFileInfoView">
                             <h1>Metadata Form<span></span></h1>
-                              <App url_schema={OC.generateUrl('/apps/irods_meta/api/schema/' + this.path)}
-                                   url_data={OC.generateUrl('/apps/irods_meta/api/meta/' + this.path)}
-                                   url_submit={OC.generateUrl('/apps/irods_meta/api/submit/' + this.path)} />
+                              <App url_schema={OC.generateUrl('/apps/irods_meta/api/schema' + this.path)}
+                                   url_data={OC.generateUrl('/apps/irods_meta/api/meta' + this.path)}
+                                   url_submit={OC.generateUrl('/apps/irods_meta/api/submit' + this.path)} />
                             </div>
                             <a className="close icon-close" href="#" alt="Close"/>
                          </div>;
